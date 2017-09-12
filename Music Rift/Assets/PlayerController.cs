115c67da6bb2b85200cfcc6f484ee5a4d84f4a95
MaxiKvaksi <maksim_dadonov4@mail.ru>
2017-09-12 20:45:53 +0400

    Smth

---
+++ Music Rift/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public int speed;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
+    }
+}

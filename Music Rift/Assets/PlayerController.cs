891456c52418c52b1635354d0a6a1d023249e6d6
MaxiKvaksi <maksim_dadonov4@mail.ru>
2017-09-12 22:18:15 +0400

    Движение перса

--- Music Rift/Assets/PlayerController.cs
+++ Music Rift/Assets/PlayerController.cs
@@ -6,12 +6,12 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
     public int speed;
+    bool isGrounded = true;
 
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-		
+        rb = GetComponent<Rigidbody2D>();	
 	}
 	
 	// Update is called once per frame
@@ -24,6 +24,39 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
+        rb.transform.Translate(new Vector2(moveHorizontal, 0.0f) * Time.deltaTime * speed);
+
+        if (isGrounded)
+        {
+            //if (Input.GetKey(KeyCode.Space))
+                rb.AddForce(new Vector2(0, moveVertical) * 100);
+        }
+        //rb.AddForce(new Vector2(moveHorizontal, moveVertical) * speed);
     }
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+        Debug.Log("Enter");
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+        Debug.Log("Exit");
+    }
+
+   /* void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }*/
 }
